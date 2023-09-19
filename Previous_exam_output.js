0a2888044c8cdec72a80ec986725f9342b2a16a6
NS-Bae <qw9711@gmail.com>
2023-09-19 20:44:09 +0900

    기출문제 시험결과 페이지 완성 및 영단어 시험페이지 수정

--- Previous_exam_output.js
+++ Previous_exam_output.js
@@ -4,5 +4,99 @@ var labelText = document.querySelector(".board label");
 subject.addEventListener('change', function()
 {
     var option = subject.options[subject.selectedIndex].text;
-    labelText.textContent = option + " 시험 결과";
+    var board = document.querySelector(".board");
+    var T_container = document.querySelector(".T_container");
+    var loading = document.querySelector(".loading_container");
+    var select_problem = document.querySelector(".select_problem");
+    var compare_form = document.querySelector(".compare_form");
+    var wrong = document.getElementById("wrong");
+
+    if(option == "국어" ||option == "영어" ||option == "수학" ||option == "사회" ||option == "과학")
+    {
+        T_container.style.height = "65%";
+        select_problem.style.height = "25%";
+        board.style.display = "flex";
+        labelText.textContent = option + " 시험 결과";
+        loading.style.display = "flex";
+        compare_form.style.display = "flex";
+        wrong.style.display = "flex";
+
+        switch(option)
+        {
+            case "국어" :
+                updateLoadingBar(0, 95);
+                updateRate(87.5, 95);
+                break;
+            case "영어" :
+                updateLoadingBar(0, 80);
+                updateRate(80, 80);
+                break;
+            case "수학" :
+                updateLoadingBar(0, 70);
+                updateRate(87.5, 70);
+                break;
+            case "사회" :
+                updateLoadingBar(0, 50);
+                updateRate(87.5, 50);
+                break;
+            case "과학" :
+                updateLoadingBar(0, 99);
+                updateRate(87.5, 99);
+                break;
+        }
+    }
+    else if(option == "선택하세요")
+    {
+        T_container.style.height = "65%";
+        select_problem.style.height = "25%";
+        board.style.display = "flex";
+        labelText.textContent = "시험 결과";
+        loading.style.display = "none";
+        compare_form.style.display = "none";
+        wrong.style.display = "none";
+    }
+    
 });
+
+/*로딩바*/ 
+const loadingBar = document.querySelectorAll(".bar");
+const progressText = document.getElementById("per");
+const total_rate = document.getElementById("total_rate");
+const my_rate = document.getElementById("my_rate");
+const explain = document.getElementById("explain");
+const gap = document.getElementById("gap");
+const explain1 = document.getElementById("explain1");
+
+function updateLoadingBar(index, percentage) 
+{
+    loadingBar[index].style.width = percentage + "%";
+    progressText.textContent = percentage + "%";
+}
+
+function updateRate(Tpercentage, Mpercentage)
+{
+    total_rate.textContent = "전체 정답률 : " +  Tpercentage + "%";
+    my_rate.textContent = "나의 정답률 : " + Mpercentage + "%";
+    explain.textContent = "당신은 평균 정답률보다";
+    explain1.textContent = "";
+    gap.style.display = "flex";
+
+    if(Tpercentage>Mpercentage)
+    {
+        var gapPercent = parseInt(Tpercentage - Mpercentage);
+        gap.textContent = gapPercent + "%";
+        explain1.textContent = "낮습니다.";
+    }
+    else if(Tpercentage<Mpercentage)
+    {
+        var gapPercent = parseInt(Mpercentage - Tpercentage);
+        gap.textContent = gapPercent + "%";
+        explain1.textContent = "높습니다.";
+    }
+    else if(Tpercentage == Mpercentage)
+    {
+        explain.textContent = "당신은 평균 정답률과";
+        gap.style.display = "none";
+        explain1.textContent = "같습니다.";
+    }
+}
--- Word_exam_output.js
+++ Word_exam_output.js
@@ -1,111 +1,161 @@
-var level = document.getElementById("level");
-var labelText = document.querySelector(".board label");
-
-level.addEventListener('change', function()
-{
-    var option = level.options[level.selectedIndex].text;
-    var board = document.querySelector(".board");
-    var T_container = document.querySelector(".T_container");
-    var loading = document.querySelector(".loading_container");
-    var select_problem = document.querySelector(".select_problem");
-    var compare_form = document.querySelector(".compare_form");
-    var wrong = document.getElementById("wrong");
-
-    if(option == "초등" ||option == "중등" ||option == "고등" ||option == "토익")
-    {
-        T_container.style.height = "65%";
-        select_problem.style.height = "25%";
-        board.style.display = "flex";
-        labelText.textContent = option + " 영단어 시험 결과";
-        loading.style.display = "flex";
-        compare_form.style.display = "flex";
-        wrong.style.display = "flex";
-
-        switch(option)
-        {
-            case "초등" :
-                updateLoadingBar(0, 95);
-                updateRate(87.5, 95);
-                break;
-            case "중등" :
-                updateLoadingBar(0, 80);
-                updateRate(80, 80);
-                break;
-            case "고등" :
-                updateLoadingBar(0, 70);
-                updateRate(87.5, 70);
-                break;
-            case "토익" :
-                updateLoadingBar(0, 50);
-                updateRate(87.5, 50);
-                break;
-        }
-    }
-    else if(option == "선택하세요")
-    {
-        T_container.style.height = "65%";
-        select_problem.style.height = "25%";
-        board.style.display = "flex";
-        labelText.textContent = "시험 결과";
-        loading.style.display = "none";
-        compare_form.style.display = "none";
-        wrong.style.display = "none";
-    }
-    else
-    {
-        T_container.style.height = "85%";
-        select_problem.style.height = "7%";
-        board.style.display = "none";
-        loading.style.display = "flex";
-        compare_form.style.display = "flex";
-        wrong.style.display = "flex";
-        total_result();
-    }
-});
-
-function total_result()
-{
-
-}
-/*로딩바*/ 
-const loadingBar = document.querySelectorAll(".bar");
+// HTML에서 요소들을 가져옴
+const levelSelect = document.getElementById("level");
+const labelText = document.querySelector(".board label");
+const loadingBars = document.querySelectorAll(".bar");
 const progressText = document.getElementById("per");
-const total_rate = document.getElementById("total_rate");
-const my_rate = document.getElementById("my_rate");
+const totalRate = document.getElementById("total_rate");
+const myRate = document.getElementById("my_rate");
 const explain = document.getElementById("explain");
 const gap = document.getElementById("gap");
 const explain1 = document.getElementById("explain1");
+const scoreSection = document.getElementById("score_section");
+const wrongSection = document.getElementById("wrong_section");
+const compareZone = document.getElementById("compare_zone");
+const const_rate = document.getElementById("const_rate");
+const ele_load = document.getElementById("ele_load");
+const mid_load = document.getElementById("mid_load");
+const high_load = document.getElementById("high_load");
+const toeic_load = document.getElementById("toeic_load");
+const result_list = document.getElementById("result_list");
+const TContainer = document.querySelector(".T_container");
+const selectProblem = document.querySelector(".select_problem");
+const loading = document.querySelector(".loading_container");
+const compareForm = document.querySelector(".compare_form");
+const wrong = document.getElementById("wrong");
+const ele_h4 = document.getElementById("ele");
+const mid_h4 = document.getElementById("mid");
+const high_h4 = document.getElementById("high");
+const toeic_h4 = document.getElementById("toeic");
+const ele_rate = document.getElementById("ele_rate");
 
-function updateLoadingBar(index, percentage) 
-{
-    loadingBar[index].style.width = percentage + "%";
-    progressText.textContent = percentage + "%";
-}
+    // 초기에는 점수를 숨김
+scoreSection.style.display = "none";
+ele_load.style.display = "none";
+mid_load.style.display = "none";
+high_load.style.display = "none";
+toeic_load.style.display = "none";
 
-function updateRate(Tpercentage, Mpercentage)
-{
-    total_rate.textContent = "전체 정답률 : " +  Tpercentage + "%";
-    my_rate.textContent = "나의 정답률 : " + Mpercentage + "%";
-    explain.textContent = "당신은 평균 정답률보다";
-    explain1.textContent = "";
-    gap.style.display = "flex";
+    // 레벨 선택 이벤트 리스너
+    levelSelect.addEventListener('change', function () 
+    {
+        const option = levelSelect.options[levelSelect.selectedIndex].text;
+        if (option === "초등" || option === "중등" || option === "고등" || option === "토익") 
+        {
+            TContainer.style.height = "65%";
+            selectProblem.style.height = "25%";
+            labelText.textContent = option + " 영단어 시험 결과";
+            loading.style.display = "flex";
+            compareForm.style.display = "flex";
+            wrong.style.display = "flex";
+            const_rate.style.display = "flex";
+            compareZone.style.display = "flex";
+            result_list.style.display = "flex";
+            wrongSection.style.display = "flex";
+            
+            scoreSection.style.display = "none";
+            ele_load.style.display = "none";
+            mid_load.style.display = "none";
+            high_load.style.display = "none";
+            toeic_load.style.display = "none";
 
-    if(Tpercentage>Mpercentage)
+            switch (option) {
+                case "초등":
+                    updateLoadingBar(0, 95);
+                    updateRate(87.5, 95);
+                    break;
+                case "중등":
+                    updateLoadingBar(0, 80);
+                    updateRate(80, 80);
+                    break;
+                case "고등":
+                    updateLoadingBar(0, 70);
+                    updateRate(87.5, 70);
+                    break;
+                case "토익":
+                    updateLoadingBar(0, 50);
+                    updateRate(87.5, 50);
+                    break;
+            }
+        } 
+        else if (option === "선택하세요") 
+        {
+            TContainer.style.height = "65%";
+            selectProblem.style.height = "25%";
+            labelText.textContent = "시험 결과";
+            loading.style.display = "none";
+            compareForm.style.display = "none";
+            wrong.style.display = "none";
+        } 
+        else 
+        {
+            totalResult();
+            changeHead();
+            updateLoadingBar(0, 95);
+        }
+    });
+
+    // 점수를 보여주는 함수
+    function totalResult() 
     {
-        var gapPercent = parseInt(Tpercentage - Mpercentage);
-        gap.textContent = gapPercent + "%";
-        explain1.textContent = "낮습니다.";
+        TContainer.style.height = "85%";
+        selectProblem.style.height = "7%";
+
+        loading.style.display = "flex";
+        compareForm.style.display = "flex";
+        wrong.style.display = "flex";
+        result_list.style.display = "none";
+        wrongSection.style.display = "none";
+        compareZone.style.display = "none";
+        const_rate.style.display = "none";
+        scoreSection.style.display = "flex";
+        ele_load.style.display = "flex";
+        mid_load.style.display = "flex";
+        high_load.style.display = "flex";
+        toeic_load.style.display = "flex";
+        ele_rate.style.display = "flex";
     }
-    else if(Tpercentage<Mpercentage)
+    function changeHead()
     {
-        var gapPercent = parseInt(Mpercentage - Tpercentage);
-        gap.textContent = gapPercent + "%";
-        explain1.textContent = "높습니다.";
+        ele_h4.textContent = "초등 영단어 결과"
+        mid_h4.textContent = "중등 영단어 결과"
+        high_h4.textContent = "고등 영단어 결과"
+        toeic_h4.textContent = "토익 영단어 결과"
     }
-    else if(Tpercentage == Mpercentage)
+
+    // 로딩 바 업데이트 함수
+    function updateLoadingBar(index, percentage) 
     {
-        explain.textContent = "당신은 평균 정답률과";
-        gap.style.display = "none";
-        explain1.textContent = "같습니다.";
+        loadingBars[index].style.width = percentage + "%";
+        progressText.textContent = percentage + "%";
     }
-}
+
+    // 정답률 업데이트 함수
+    function updateRate(Tpercentage, Mpercentage) 
+    {
+        totalRate.textContent = "전체 정답률 : " + Tpercentage + "%";
+        myRate.textContent = "나의 정답률 : " + Mpercentage + "%";
+        explain.textContent = "당신은 평균 정답률보다";
+        explain1.textContent = "";
+        gap.style.display = "flex";
+
+        if (Tpercentage > Mpercentage) 
+        {
+            const gapPercent = parseInt(Tpercentage - Mpercentage);
+            gap.textContent = gapPercent + "%";
+            explain1.textContent = "낮습니다.";
+            console.log(gapPercent);
+        } 
+        else if (Tpercentage < Mpercentage) 
+        {
+            const gapPercent = parseInt(Mpercentage - Tpercentage);
+            gap.textContent = gapPercent + "%";
+            explain1.textContent = "높습니다.";
+            console.log(gapPercent);
+        } 
+        else if (Tpercentage === Mpercentage) 
+        {
+            explain.textContent = "당신은 평균 정답률과";
+            gap.style.display = "none";
+            explain1.textContent = "같습니다.";
+        }
+    }
