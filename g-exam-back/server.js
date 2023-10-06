4df01a4f9a9a66b0042d3c480d18f326baf5cce7
NS-Bae <qw9711@gmail.com>
2023-10-06 21:58:57 +0900

    express 세팅, DB연결

--- g-exam-back/server.js
+++ g-exam-back/server.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const mysql = require('mysql');
 const path = require('path');
-const app = express();
 
-app.listen(4000, function() {
-    console.log('listening on 4000')
-    }
-);
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
+});
 app.use(express.static(path.join(__dirname, '../g-exam-front/build')));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../g-exam-front/build/index.html'));
 });
+
+const connection = mysql.createConnection({
+    host : 'localhost', 
+    user : 'root', 
+    password : 'My19971108!', 
+    database : 'g-exam'
+});
+
+connection.connect();
+
+connection.query('SELECT * from user_student where (grade = 1)', (error, rows, fields) => {
+    if (error) throw error;
+    console.log('User info is: ', rows);
+  });
+  
+  connection.end();
