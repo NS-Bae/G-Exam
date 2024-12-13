947af491fd419e24fe36e99d67460b74f9fa72dc
NS-Bae <qw9711@gmail.com>
2024-12-13 21:47:16 +0900

    주관식문제 테스트

--- g-exam-front/src/WorkbookExam_English_Listening.js
+++ g-exam-front/src/WorkbookExam_English_Listening.js
@@ -333,7 +333,7 @@ function EssayExamForm({
   selectedAnswer }) 
 {
   const key = `${result.classification_name}/${result.exam_id}`;
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState(null);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
