c63eb2544f69843ed4e6a41b975de3d733289c47
NS-Bae <qw9711@gmail.com>
2024-12-13 21:49:07 +0900

    주관식 입력 원상복구

--- g-exam-front/src/WorkbookExam_English_Listening.js
+++ g-exam-front/src/WorkbookExam_English_Listening.js
@@ -333,7 +333,7 @@ function EssayExamForm({
   selectedAnswer }) 
 {
   const key = `${result.classification_name}/${result.exam_id}`;
-  const [inputValue, setInputValue] = useState(null);
+  const [inputValue, setInputValue] = useState(selectedAnswer || '');
 
   const handleInputChange = (e) => {
     const value = e.target.value;
