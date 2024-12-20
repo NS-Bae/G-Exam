62aef6b17b6b34a4c42349610dca6cd9f0a94833
NS-Bae <qw9711@gmail.com>
2024-12-20 21:04:34 +0900

    객관식 문제발생

--- g-exam-back/routes.js
+++ g-exam-back/routes.js
@@ -1878,7 +1878,7 @@ router.post('/api/submit_exam_answer', async (req, res) => {
     } 
     else 
     {
-      console.log('qqq', result)
+      console.log('qqq', result, query, value);
       const objectiveQuestions = result.filter((item) => item.type === '객관식');
       const updatedObjectiveQuestions = objectiveQuestions.map((question) => {
         const answerNumber = parseInt(question.answer);
